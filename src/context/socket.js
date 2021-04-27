c0e53c02fc774915d7ea2793798cacc4d0ff5081
dbelmo2 <danny.belmonte4@gmail.com>
2021-04-27 16:17:30 -0500

    connected to remote http server successfully

--- src/context/socket.js
+++ src/context/socket.js
@@ -1,5 +1,5 @@
 import socketio from "socket.io-client";
 import React from 'react';
 
-export const socket = socketio.connect("https://192.168.0.14:8443");
+export const socket = socketio.connect("http://161.35.53.205:8000");
 export const SocketContext = React.createContext();
